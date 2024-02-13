0b8508537c0716ce1a953e565c918415156aca0f
Thibaud <tibo.hrn@gmail.com>
2024-02-13 15:55:13 +0100

    Mise en forme
    Mise en forme boutton bots et div bot

--- src/views/bots.js
+++ src/views/bots.js
@@ -2,8 +2,8 @@ export default () => (`
 <div class="chat-container">
 <div class="bot-list">
     <!-- Liste des bots -->
-    <div class="bot">Bot 1</div>
-    <div class="bot">Bot 2</div>
-    <div class="bot">Bot 3</div>
+    <div class="bot"><img src="https://i-dj.unimedias.fr/2023/09/12/dj115chatonth-6500273acbf37.jpg?auto=format%2Ccompress&crop=faces&cs=tinysrgb&fit=max&w=680">  Nom du premier bot</div>
+    <div class="bot"><img src="https://i-dj.unimedias.fr/2023/09/12/dj115chatonth-6500273acbf37.jpg?auto=format%2Ccompress&crop=faces&cs=tinysrgb&fit=max&w=680">  Nom du second bot</div>
+    <div class="bot"><img src="https://i-dj.unimedias.fr/2023/09/12/dj115chatonth-6500273acbf37.jpg?auto=format%2Ccompress&crop=faces&cs=tinysrgb&fit=max&w=680">Nom du troisième bot</div>
 </div>
 `);
