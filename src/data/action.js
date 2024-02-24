d8958d1916ba67affce6baaf2497a28c5851fd36
DIGYSKY <lilyan.chauveau@gmail.com>
2024-02-24 03:08:15 +0100

    alimante model action.js

--- src/data/action.js
+++ src/data/action.js
@@ -1,15 +1,29 @@
 export default [
   {
     name: 'Bonjour',
+    accordCocordence: true,
     who: 'all',
     description: 'Dire bonjour aux bots',
-    keyWord: ['Bonjour', 'bonjour', 'Hello', 'hello'],
+    keyWord: ['bonjour', 'hello'],
     action: () => ('Bonjour !')
   }, {
+    name: 'Clear',
+    accordCocordence: false,
+    who: 'all',
+    description: 'Supprimer les messages',
+    keyWord: ['clear'],
+    action: () => {
+      const messageBox = document.getElementById('message-box');
+      messageBox.innerHTML = '';
+
+      return ('Messages supprimer !');
+    }
+  }, {
     name: 'Insultes',
+    accordCocordence: true,
     who: 'all',
     description: 'Dire bonjour aux bots',
-    keyWord: ['Connard', 'CONNARD', 'connard', 'fdp', 'FDP', 'ferailleur'],
-    action: () => ('Fils à pute ! Ta mère suce des Ours !!!')
+    keyWord: ['connard', 'fdp', 'pute', 'salope', 'enculer', 'pouilleux'],
+    action: () => ('Les insultes ne pas acceptable !')
   }
 ];
