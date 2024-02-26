f94d31b65763f821cc79d5a1cfb0d280d36157d7
DIGYSKY <lilyan.chauveau@gmail.com>
2024-02-26 15:56:24 +0100

    add img

--- src/data/botsList.js
+++ src/data/botsList.js
@@ -2,24 +2,28 @@ export default [
   {
     index: 1,
     title: 'Bot I',
+    img: 'https://static.vecteezy.com/ti/vecteur-libre/p1/10054157-chat-bot-robot-avatar-en-cercle-forme-ronde-isole-sur-fond-blanc-illustrationle-de-stock-technologie-ai-futuriste-aide-communication-conversation-concept-dans-un-style-plat-vectoriel.jpg',
     description: 'description du chatbot',
     statut: true
   },
   {
     index: 2,
     title: 'Bot II',
+    img: 'https://static.vecteezy.com/ti/vecteur-libre/p1/10054157-chat-bot-robot-avatar-en-cercle-forme-ronde-isole-sur-fond-blanc-illustrationle-de-stock-technologie-ai-futuriste-aide-communication-conversation-concept-dans-un-style-plat-vectoriel.jpg',
     description: 'description du chatbot',
     statut: false
   },
   {
     index: 3,
     title: 'Bot III',
+    img: 'https://static.vecteezy.com/ti/vecteur-libre/p1/10054157-chat-bot-robot-avatar-en-cercle-forme-ronde-isole-sur-fond-blanc-illustrationle-de-stock-technologie-ai-futuriste-aide-communication-conversation-concept-dans-un-style-plat-vectoriel.jpg',
     description: 'description du chatbot',
     statut: true
   },
   {
     index: 4,
     title: 'Bot IV',
+    img: 'https://static.vecteezy.com/ti/vecteur-libre/p1/10054157-chat-bot-robot-avatar-en-cercle-forme-ronde-isole-sur-fond-blanc-illustrationle-de-stock-technologie-ai-futuriste-aide-communication-conversation-concept-dans-un-style-plat-vectoriel.jpg',
     description: 'description du chatbot',
     statut: true
   }
