f7f971e7c9e9ddec07c8372d4f7dd3f4aaf72b09
DIGYSKY <lilyan.chauveau@gmail.com>
2024-02-26 15:55:07 +0100

    add image and title

--- src/views/bots.js
+++ src/views/bots.js
@@ -4,7 +4,7 @@ export default (bot) => (`
   <div class="row g-0 align-items-center">
     <div class="col-md-3">
       <img
-        src="https://static.vecteezy.com/ti/vecteur-libre/p1/10054157-chat-bot-robot-avatar-en-cercle-forme-ronde-isole-sur-fond-blanc-illustrationle-de-stock-technologie-ai-futuriste-aide-communication-conversation-concept-dans-un-style-plat-vectoriel.jpg"
+        src="${bot.img}"
         class="img-fluid rounded-start card-img-top" alt="...">
     </div>
     <div class="col-md-8">
