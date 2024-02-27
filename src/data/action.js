bdd40ea5c1e1aab6d4d6168d4b61505e1ca542bf
DIGYSKY <lilyan.chauveau@gmail.com>
2024-02-27 20:12:39 +0100

    add parameters to display message

--- src/data/action.js
+++ src/data/action.js
@@ -2,13 +2,15 @@ export default [
   {
     name: 'Bonjour',
     accordCocordence: true,
+    history: true,
     who: 'all',
     description: 'Dire bonjour aux bots',
-    keyWord: ['bonjour', 'hello'],
+    keyWord: ['bonjour', 'hello', 'salut'],
     action: () => ('Bonjour !')
   }, {
     name: 'Clear',
     accordCocordence: false,
+    history: false,
     who: 'all',
     description: 'Supprimer les messages',
     keyWord: ['clear'],
@@ -16,11 +18,14 @@ export default [
       const messageBox = document.getElementById('message-box');
       messageBox.innerHTML = '';
 
+      localStorage.removeItem('messageHistory');
+
       return ('Messages supprimer !');
     }
   }, {
     name: 'Insultes',
     accordCocordence: true,
+    history: true,
     who: 'all',
     description: 'Dire bonjour aux bots',
     keyWord: ['connard', 'fdp', 'pute', 'salope', 'enculer', 'pouilleux'],
